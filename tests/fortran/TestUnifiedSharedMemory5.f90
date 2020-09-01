7be7c4e5505f8a6b9e0172e70ec285c5f90c08c4
jz10 <root@vm-21b-02h23.novalocal>
2020-09-02 01:24:08 +0800

    add

--- tests/fortran/TestUnifiedSharedMemory5.f90
+++ tests/fortran/TestUnifiedSharedMemory5.f90
@@ -1,8 +1,8 @@
-! Test the shared arrray with target related constructs and directives
-
+! Test the shared used defined type arrray with target related constructs and directives
+  
 PROGRAM Test_OMP_Unified_Shared_Memory
   IMPLICIT NONE
-  !!$OMP REQUIRES UNIFIED_SHARED_MEMORY               
+  !$OMP REQUIRES UNIFIED_SHARED_MEMORY               
   INTEGER, PARAMETER :: SIZE = 100
   REAL :: B(SIZE), C(SIZE)
   INTEGER :: i
@@ -17,20 +17,26 @@ PROGRAM Test_OMP_Unified_Shared_Memory
 
   DO i = 1, SIZE, 1
      B(i) = i
-     C(i) = i
      D(i)%i = i
      D(i)%f = i * 0.1
   END DO
 
-  x = Test_Function(B, C, SIZE, 10, 10, 10)
+  x = Test_Function(B, D, SIZE, 10, 10, 10)
   PRINT *, 'Test result:  ', x
 END PROGRAM Test_OMP_Unified_Shared_Memory
 
-function Test_Function(B,C,N, block_size, num_teams, block_threads) result(sum)
+function Test_Function(B, D, N, block_size, num_teams, block_threads) result(sum)
   implicit none
-  real    :: B(N), C(N), sum
+  real    :: B(N), sum
   integer :: N, block_size, num_teams, block_threads, i, i0
-  
+
+  TYPE TestStruct
+     INTEGER :: i
+     REAL    :: f
+  END TYPE TestStruct
+
+  type(TestStruct) :: D(N)
+
   sum = 0.0e0
   !$omp target 
   !$omp teams num_teams(num_teams) thread_limit(block_threads) &
@@ -39,10 +45,8 @@ function Test_Function(B,C,N, block_size, num_teams, block_threads) result(sum)
   do i0=1,N, block_size
      !$omp parallel do reduction(+:sum)
      do i = i0, min(i0+block_size,N)
-        sum = sum + B(i) * C(i)
+        sum = sum + B(i) * D(i)%f
      end do
-
-     C(i0) = sum
   end do
   !$omp end teams
   !$omp end target
